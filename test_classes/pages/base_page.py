cc2770e455c14c06a92049599f64f0605bf6cb4f
Lucas Gabriel <lucas.gabriel3.lg@gmail.com>
2025-07-21 20:05:59 -0300

    created basepage

--- test_classes/pages/base_page.py
+++ test_classes/pages/base_page.py
@@ -1,2 +1,28 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
+
 class BasePage:
-    pass
+
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    def click(self, by, value):
+        element = self.driver.find_element(by, value)
+        element.click()
+
+    def input(self, by, value, text):
+        element = self.driver.find_element(by, value)
+        element.send_keys(text)
+
+    def wait_until_element_is_visible(self, by, value, timeout):
+        WebDriverWait(self.driver, timeout=timeout).until(expected_conditions.visibility_of_element_located((by, value)))
+
+    def wait_and_click(self, by, value, timeout):
+        element = WebDriverWait(self.driver, timeout=timeout).until(expected_conditions.visibility_of_element_located((by, value)))
+        element.click()
+
+    def confirm_element_is_displayed(self, by, value):
+        element = WebDriverWait(self.driver, timeout=20).until(expected_conditions.visibility_of_element_located((by, value)))
+        return element.is_displayed()
